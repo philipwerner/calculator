4dafe2a604ede78929a46e6d4574869803a09fb5
philipwerner <philip.r.werner@gmail.com>
2018-03-18 21:29:16 -0700

    first run through, time to refactor

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Global Variables
+            int num1;
+            int num2;
+            int answer;
+            string oper;
+
+            Console.WriteLine("Calculator!");
+
+            //Getting all the user info
+            Console.WriteLine("Enter your first integer: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your second integer: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Now enter your operator (x + - /): ");
+            oper = Console.ReadLine();
+
+            //Running the math based on the operator
+            if (oper == "x")
+            {
+                answer = num1 * num2;
+                Console.WriteLine(num1 + " x " + num2 + " = " + answer);
+                Console.ReadLine();
+            }
+
+            else if (oper == "-")
+            {
+                answer = num1 - num2;
+                Console.WriteLine(num1 + " - " + num2 + " = " + answer);
+                Console.ReadLine();
+            }
+
+            else if (oper == "/")
+            {
+                answer = num1 / num2;
+                Console.WriteLine(num1 + " / " + num2 + " = " + answer);
+                Console.ReadLine();
+            }
+
+            else if (oper == "+")
+            {
+                answer = num1 + num2;
+                Console.WriteLine(num1 + " + " + num2 + " = " + answer);
+                Console.ReadLine();
+            }
+
+            else
+            {
+                Console.WriteLine("Could not understand the operator");
+                Console.ReadLine();
+            }
+        }
+    }
+}
